7c311706c1d71c41c1b97db76b3b30c6af36efbf
Shawn Ren <shawnren527@gmail.com>
2019-04-11 02:08:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,6 @@ class Student
 
   def self.first_student_in_grade_10
     self.new_from_db(DB[:conn].execute('SELECT * FROM students WHERE grade = 10 ORDER BY grade LIMIT ?', x).flatten)
-
   end
 
 
